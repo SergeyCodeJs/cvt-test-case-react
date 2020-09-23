06daae723c27fc4759bfc4bfad1ffa7283ef9900
SergeyCodeJs <sergeysteh@gmail.com>
2020-09-23 18:32:33 +0400

    fix: chanels component scss for pixel perfect

--- src/components/channels/channels-container/channel/schedule/schedule.js
+++ src/components/channels/channels-container/channel/schedule/schedule.js
@@ -4,7 +4,7 @@ import classes from './schedule.module.scss'
 export default function Schedule({time, title}) {
     return (
         <div className={classes.channel__program_container}>
-            <p className={classes.channel__time}>${time}</p>
+            <p className={classes.channel__time}>{time}</p>
             <p className={classes.channel__program_title}>{title}</p>
         </div>
     )
