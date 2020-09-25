e7ef63bc20a368e93e555f185bc925b39197cda3
SergeyCodeJs <sergeysteh@gmail.com>
2020-09-25 15:53:53 +0400

    feat: add description show on image mouse enter

--- src/components/movies/singleMovie/singleMovie.js
+++ src/components/movies/singleMovie/singleMovie.js
@@ -1,14 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
 import classes from '../singleMovie/singleMovie.module.scss'
 
 export default function SingleMovie({movieName, logoSrc, description}) {
-    const {movie__container, movie, movie__img, movie__description, movie__title} = classes;
-    
+    const {
+        movie__container,
+        movie,
+        movie__img,
+        movie__description,
+        movie__title,
+        low_brightness
+    } = classes;
+
+    const [showDescription,
+        setShowDescription] = useState(false);
+
+    function onHoverHandler(e) {
+        if (e.type === 'mouseenter') {
+            setShowDescription(true);
+
+        } else {
+            setShowDescription(false);
+        }
+    }
+
+    const cls = showDescription
+        ? [movie__img, low_brightness]
+        : [movie__img];
+
     return (
         <div className={movie__container}>
-            <div className={movie}>
-                <img className={movie__img} src={logoSrc} alt={movieName}></img>
-                <p className={movie__description}>{description}</p>
+            <div
+                onMouseLeave={onHoverHandler}
+                onMouseEnter={onHoverHandler}
+                className={movie}>
+                <img className={cls.join(' ')} src={logoSrc} alt={movieName}></img>
+                {showDescription
+                    ? <p className={movie__description}>{description}</p>
+                    : null}
             </div>
             <p className={movie__title}>{movieName}</p>
         </div>
