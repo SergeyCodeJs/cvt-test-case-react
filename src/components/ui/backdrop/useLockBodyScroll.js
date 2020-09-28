4231188c1ecce27182df7e34cce22bea9e1ea7a9
SergeyCodeJs <sergeysteh@gmail.com>
2020-09-28 13:31:30 +0400

    fix: prevent body goes left when modal open

--- src/components/ui/backdrop/useLockBodyScroll.js
+++ src/components/ui/backdrop/useLockBodyScroll.js
@@ -5,7 +5,27 @@ export default function useLockBodyScroll() {
         const originalStyle = window
             .getComputedStyle(document.body)
             .overflow;
+
+        const scrollWidth = calcScroll();
         document.body.style.overflow = 'hidden';
-        return () => document.body.style.overflow = originalStyle;
+        document.body.style.marginRight = scrollWidth + 'px';
+        return () => {
+          document.body.style.marginRight = "";
+          document.body.style.overflow = originalStyle;
+        }
     }, []);
+}
+
+function calcScroll() {
+  let div = document.createElement('div');
+
+  div.style.overflowY = 'scroll';
+  div.style.width = '50px';
+  div.style.height = '50px';
+  
+  document.body.append(div);
+  let scrollWidth = div.offsetWidth - div.clientWidth;
+  
+  div.remove();
+  return scrollWidth
 }
